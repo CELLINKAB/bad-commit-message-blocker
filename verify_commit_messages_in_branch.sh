4c6605d1d76e47cabd9750e702e948db5b25524a
Ricardo Oliveira <ricardo.m.oliv@gmail.com>
2020-06-08 17:48:13 +0200

    Allow specifying base branch

--- verify_commit_messages_in_branch.sh
+++ verify_commit_messages_in_branch.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # A helper script to verify the messages of every commit in
-# the current branch when compared against `origin/master`.
+# the current branch when compared against `origin/<base_branch>`.
 # If there is no difference then the latest commit will be used.
+
+if [ -z "$1" ]
+then
+	echo "Base branch not specified, falling back to master"
+	base_branch=master
+fi
+
 set -eu
 
 script_dir=$(dirname "$0")
-commits_since_master=$(git rev-list HEAD ^origin/master)
+base_branch=$1
+commits_since_base=$(git rev-list HEAD ^origin/$base_branch)
 
 while read -r commit_hash; do
     commit_message="$(git log --format=%B -n 1 $commit_hash)"
     python3 $script_dir/bad_commit_message_blocker.py --message "$commit_message"
-done <<< "$commits_since_master"
+done <<< "$commits_since_base"
